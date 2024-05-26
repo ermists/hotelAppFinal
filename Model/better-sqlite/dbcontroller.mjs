41d043a960f18a914523e04cc65dc7b85bb50806
ermists <up1084066@ac.upatras.gr>
2024-05-26 03:37:33 +0300

    log in out COMPLETED

--- Model/better-sqlite/dbcontroller.mjs
+++ Model/better-sqlite/dbcontroller.mjs
@@ -1,12 +1,10 @@
 import db from 'better-sqlite3';
 import bcrypt from 'bcrypt';
 
-const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
-
-
 
 export function addNewRes (name, surname, SSN, street, city, postalCode, email, telephone, arrivalDate, departureDate, room,peopleNo) {
     try {
+        const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
         let max = 18736534;
         let randomInt = Math.floor(Math.random() * max);
         const stmt = sql.prepare('INSERT INTO "RESERVATION" VALUES (?, ?, ?, ?, ?, ?)');
@@ -14,7 +12,9 @@ export function addNewRes (name, surname, SSN, street, city, postalCode, email, 
 
         const stmt2 = sql.prepare('INSERT INTO "USER" VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
         stmt2.run(SSN, name, surname, street, city, postalCode, email, telephone);
+        sql.close();
     }catch (err) {
+        sql.close();
         console.error('Error creating new reservation:', err);
         throw(err);
     }
@@ -22,9 +22,12 @@ export function addNewRes (name, surname, SSN, street, city, postalCode, email, 
 
 export function changeResDate (roomNo, custEmail, oldArrivalDate, newArrivalDate, newDepartureDate) {
     try {
+        const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
         const stmt = sql.prepare('UPDATE "RESERVATION" SET "Arrival" = ? AND "Departure" = ? WHERE "RoomNumber" = ? AND "Arrrival" = ? AND SSN IN (SELECT SSN FROM "USER" WHERE "Email" = ?)');
         stmt.run(newArrivalDate, newDepartureDate, roomNo, oldArrivalDate, custEmail);
+        sql.close();
     }catch (err) {
+        sql.close();
         console.error('Error changing reservation date:', err);
         throw(err);
     }
@@ -32,9 +35,12 @@ export function changeResDate (roomNo, custEmail, oldArrivalDate, newArrivalDate
 
 export function changeResRoom (roomNo, newRoomNo, custEmail, arrivalDate) {
     try {
+        const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
         const stmt = sql.prepare('UPDATE "RESERVATION" SET "RoomNumber" = ? WHERE "RoomNumber" = ? AND SSN IN (SELECT SSN FROM "USER" WHERE "Email" = ?) AND "Arrival" = ?');
         stmt.run(newRoomNo, roomNo, custEmail, arrivalDate);
+        sql.close();
     }catch (err) {
+        sql.close();
         console.error('Error changing reservation room:', err);
         throw(err);
     }
@@ -42,61 +48,86 @@ export function changeResRoom (roomNo, newRoomNo, custEmail, arrivalDate) {
 
 export function deleteRes (roomNo, custEmail, arrivalDate) {
     try {
+        const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
         const stmt = sql.prepare('DELETE FROM "RESERVATION" WHERE "RoomNumber" = ? AND SSN IN (SELECT SSN FROM "USER" WHERE "Email" = ?) AND "Arrival" = ?');
         stmt.run(roomNo, custEmail, arrivalDate);
+        sql.close();
     }catch (err) {
+        sql.close();
         console.error('Error deleting reservation:', err);
         throw(err);
     }
 }
 
 export let findUserByUsernamePassword = async (username, password) => {
+    const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
     //Φέρε μόνο μια εγγραφή (το LIMIT 0, 1) που να έχει username και password ίσο με username και password 
     const stmt = sql.prepare('SELECT userUsername FROM "SIMPLEUSER" WHERE userUsername = ? and userPassword = ? LIMIT 0, 1');
     try {
         const user = stmt.all(username, password);
+        sql.close();
     } catch (err) {
+        sql.close();
         return false;
     }
 }
 
 export let findAdminByUsernamePassword = async (username, password) => {
+    const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
     //Φέρε μόνο μια εγγραφή (το LIMIT 0, 1) που να έχει username και password ίσο με username και password
     const stmt = sql.prepare('SELECT adminUsername FROM "ADMIN" WHERE adminUsername = ? and adminPassword = ? LIMIT 0, 1');
     try {
         const user = stmt.all(username, password);
+        sql.close();
     }
     catch (err) {
+        sql.close();
         return false;
     }
 }
 
 
-export let getAdminByUsername = (username) => {
-    const stmt = sql.prepare('SELECT AdminID, adminUsername, adminPassword FROM "ADMIN" WHERE adminUsername = ? LIMIT 0, 1');
+export let getAdminByUsername = function (username)  {
+    const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
+    const stmt = sql.prepare('SELECT * FROM "ADMIN" WHERE adminUsername = ? LIMIT 0, 1');
     try {
-        const user = stmt.all(username);
+        const user = stmt.all(String(username));
+        if (user.length==0) {
+            return undefined;
+        }
+        sql.close();    
         return user[0];
     } catch (err) {
+        sql.close();
         return false;
     }
 }
 
-export let getUserByUsername = (username) => {
-    const stmt = sql.prepare('SELECT UserID, userUsername, userPassword FROM "SIMPLEUSER" WHERE userUsername = ? LIMIT 0, 1');
+export let getUserByUsername = async function (username)  {
+    const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
+    const stmt = sql.prepare('SELECT * FROM "SIMPLEUSER" WHERE userUsername = ? LIMIT 0, 1');
     try {
-        const user = stmt.all(username);
+
+        const user = stmt.all(String(username));
+        if (user.length==0) {
+            return undefined;
+        }
+        sql.close();
         return user[0];
     } catch (err) {
+        sql.close();
         return false;
     }
 }
 
 //Η συνάρτηση δημιουργεί έναν νέο χρήστη με password
 export let registerUser = async function (username, password) {
+    const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
     // ελέγχουμε αν υπάρχει χρήστης με αυτό το username
-    const userId = getUserByUsername(username);
-    if (userId != undefined) {
+    const userId = await getUserByUsername(username);
+    console.log(userId);
+    if (userId != undefined ) {
+        sql.close();
         return { message: "Υπάρχει ήδη χρήστης με αυτό το όνομα" };
     } else {
         try {
@@ -105,7 +136,9 @@ export let registerUser = async function (username, password) {
             const hashedPassword = await bcrypt.hash(password, 10);
             const stmt = sql.prepare('INSERT INTO "SIMPLEUSER" VALUES (null,?, ?, ?)');
             const info = stmt.run(randomInt,String(username), hashedPassword);
+            sql.close();
         } catch (error) {
+            sql.close();
             console.error('Error registering user:', error);
             throw(error);
         }
@@ -113,9 +146,11 @@ export let registerUser = async function (username, password) {
 }
 
 export let registerAdmin = async function (username, password) {
+    const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
     // ελέγχουμε αν υπάρχει χρήστης με αυτό το username
     const userId = getAdminByUsername(username);
     if (userId != undefined) {
+        sql.close();
         return { message: "Υπάρχει ήδη χρήστης με αυτό το όνομα" };
     } else {
         try {
@@ -124,7 +159,9 @@ export let registerAdmin = async function (username, password) {
             const hashedPassword = await bcrypt.hash(password, 10);
             const stmt = sql.prepare('INSERT INTO "ADMIN" VALUES (null, ?, ?, ?)');
             const info = stmt.run(randomInt, String(username), hashedPassword);
+            sql.close();
         } catch (error) {
+            sql.close();
             console.error('Error registering Admin:', error);
             throw(error);
         }
@@ -133,9 +170,12 @@ export let registerAdmin = async function (username, password) {
 
 export let declareInterest = function (SSN, actSelection, date) {
     try {
+        const sql = new db('./model/db/hotelProjectDB.db',{fileMustExist: true});
         const stmt = sql.prepare('INSERT INTO "DECLARATION" VALUES (?, ?, ?)');
         stmt.run(SSN, actSelection, date);
+        sql.close();
     } catch (error) {
+        sql.close();
         console.error('Error declaring interest:', error);
         throw(error);
     }
